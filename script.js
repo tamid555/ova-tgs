0a59e1d848cb86aeb563172d6f6e81b6fdbe304c
tamid555 <132221243+tamid555@users.noreply.github.com>
2026-05-30 15:57:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -93,7 +93,7 @@ const ovas = [
     num: 16,
     topic: "Teoría de la organización y cibernética",
     author: "Kevin Yesid Urbina García",
-    link: "https://github.com/tamid555/ova-tgs/blob/main/ovas/OVA_Kevin_Urbina/TGS%20Cibernetica%20Y%20Organizacion.html"
+    link: "ovas/OVA_Kevin_Urbina/TGS%20Cibernetica%20Y%20Organizacion.html"
   },
   {
     num: 17,
